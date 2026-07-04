d9666b20938dbba23e86dbb04e11283075a628cb
Chaitanya0210 <rayewarchaitanya@gmail.com>
2026-07-04 20:40:43 +0530

    near to complete

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,12 +2,75 @@ import logo from './logo.svg';
 import './App.css';
 import Navbar from './components/Navbar';
 import Textform from './components/Textform';
+import About from './components/About';
+import Alert from './components/Alert';
+import React, { useState } from 'react';
+
+import { BrowserRouter } from "react-router";
+
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+
+
+
+
+
 
 function App() {
+
+  const [mode, setMode] = useState('light'); 
+  const [alert, setAlert] = useState(null);
+
+  const showAlert = (message, type)=>{
+    setAlert({
+      msg: message,
+      type: type
+    })
+    setTimeout(() => {
+      setAlert(null);
+    }, 2000);
+  }
+
+
+
+  const toggleMode = () => {
+    if(mode === 'light'){
+      setMode('dark');
+      showAlert("Dark mode has been enabled", "success");
+      document.title='TextUtils-Dark'
+    }
+    else{
+      setMode('light');
+      showAlert("Light mode has been enabled", "success");
+      document.title='TextUtils-Light'
+    }
+  }
+
+
+
+
+
   return (
     <>
-      <Navbar />
-      <Textform title="Give the text here"/>
+    <Router>
+    <Navbar mode={mode} toggleMode={toggleMode}/>
+    <Alert alert={alert}/>
+     <div className="container my-3">
+      <Switch>
+        <Route exact path="/about">
+          <About/>
+        </Route>
+        <Route exact path="/">
+          <Textform  showAlert={showAlert} mode={mode}/>
+        </Route>
+      </Switch>
+
+     </div>
+    </Router>
     </>
   );
 }
--- my-app/src/components/Textform.js
+++ my-app/src/components/Textform.js
@@ -4,25 +4,31 @@ export default function Textform(props) {
     const handleUpClick = ()=>{
         let newText = text.toUpperCase();
         setText(newText);
+        props.showAlert("Converted to Uppercase!","success");
     }
     
     const handleDownClick = ()=>{
         let newText = text.toLowerCase();
         setText(newText);
+        props.showAlert("Converted to Lowercase!","success");
     }
     const handleOnChange= (event)=>{
         setText(event.target.value);
+        props.showAlert("Text has been changed!","success");
     }
 
     const speakText = (text) => {
         const utterance = new SpeechSynthesisUtterance(text);
         // utterance.lang = "en-US";
         window.speechSynthesis.speak(utterance);
+        props.showAlert("Text is being spoken!","success");
     }
 
     const handleSpeak = () => {
     speakText(text);
 };
+
+
 
     const [text, setText] = useState("");
     return (
@@ -43,7 +49,39 @@ export default function Textform(props) {
             <p>{0.008 * text.split(" ").length} Minutes required to read</p> 
             <h2>Preview</h2>
             <p>{text}</p> 
+
+
+
+                <div class="btn-group" role="group" aria-label="Basic mixed styles example">
+                <button type="button" class="btn btn-danger"onClick={()=>{
+                    document.body.style.backgroundColor = "red";
+                    props.showAlert("Background color changed to red!","success");
+                    document.title='TextUtils-Red'
+                }}>Red</button>
+                <button type="button" class="btn btn-warning" onClick={()=>{
+                    document.body.style.backgroundColor = "yellow";
+                    props.showAlert("Background color changed to yellow!","success");
+                    document.title='TextUtils-Yellow'
+                }}>Yellow</button>
+                <button type="button" class="btn btn-success" onClick={()=>{
+                    document.body.style.backgroundColor = "green";
+                    props.showAlert("Background color changed to green!","success");
+                    document.title='TextUtils-Green'
+                }}>Green</button>
+                <button type="button" class="btn btn-secondary" onClick={()=>{
+                    document.body.style.backgroundColor = "grey";
+                    props.showAlert("Background color changed to white!","success");
+                    document.title='TextUtils-White'
+                }}>White</button>
+                </div>
+
+
+
             </div>
+
+            
+
+
         </>
     )
 }
